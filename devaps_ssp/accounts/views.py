a8c39a81d11504bca1911df699b2875651e0d723
Naveen Kumar <nvk.grandhi@gmail.com>
2016-03-29 11:32:16 +0530

    Adds functions to handle CRUD and also added related html files

--- devaps_ssp/accounts/views.py
+++ devaps_ssp/accounts/views.py
@@ -11,6 +11,10 @@ def home(request):
     return render(request, 'index.html')
 
 
+def admin_home(request):
+    return render(request, 'admin.html', {'user': 'admin'})
+
+
 def login_page(request):
     return render(request, 'login.html')
 
@@ -62,30 +66,76 @@ def create_user(request):
 
 
 def register(request):
-    import pdb; pdb.set_trace();
+    # import pdb; pdb.set_trace();
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            return HttpResponse('form is valid')
+            try:
+                auth_user = User()
+                auth_user.first_name = request.POST.get('first_name')
+                auth_user.last_name = request.POST.get('last_name')
+                auth_user.username = request.POST.get('username')
+                auth_user.password = request.POST.get('password')
+                auth_user.email = request.POST.get('email')
+                auth_user.is_superuser = False
+                auth_user.is_staff = True
+                auth_user.save()
+                auth_user_profile = UserProfile()
+                auth_user_profile.role = request.POST.get('role')
+                auth_user_profile.user_id = auth_user.id
+                auth_user_profile.save()
+            except:
+                error = 'Invalid Data'
+                return render(request, 'create_user.html', {'error': error})
         else:
             error = 'Pleae Fill the below details! All are mandatory'
             return render(request, 'create_user.html', {'error': error})
     else:
-        return HttpResponse('invalid request')
+        form = RegistrationForm()
+        return render(request, 'create_user.html', {'form': form})
+
+    return render(request, 'register_success.html')
 
 
 def list_users(request):
-    pass
+    # import pdb; pdb.set_trace();
+    auth_user = User.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'list_users.html', data)
 
 
-def update_user(request):
+def update(request):
+    # import pdb; pdb.set_trace();
+    auth_user = UserProfile.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'update_user.html', data)
+
+
+def update_user(request, user_id):
+    # import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        auth_user = UserProfile.objects.get(user_id=user_id)
+        auth_user.role = request.POST.get('role')
+        auth_user.save()
+
+    auth_user = UserProfile.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'update_user.html', data)
+
+
+def delete(request):
     pass
 
 
 def delete_user(request, user_id):
-    pass
+    # import pdb; pdb.set_trace()
+    auth_user = User.objects.get(pk=user_id)
+    auth_user.delete()
+    auth_user = User.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'list_users.html', data)
 
 
 def user_view(request):
-    pass
+    return render(request, 'user.html')
 
