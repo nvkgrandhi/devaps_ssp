f469bd5784ae314490dceaf491be1bb05e9d9fee
Naveen Kumar <nvk.grandhi@gmail.com>
2016-03-30 15:14:56 +0530

    Adds some more features to UI and adds function create script.py file

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^accounts_home/', views.admin_home, name='admin_home'),
+    url(r'^login_page/', views.login_page, name='login_page'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^create_user/', views.create_user, name='create_user'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^list_users/', views.list_users, name='list_users'),
+    url(r'^delete_user/(?P<user_id>\d+)$', views.delete_user, name='delete_user'),
+    url(r'^update/', views.update, name='update'),
+    url(r'^update_user/(?P<user_id>\d+)$', views.update_user, name='update_user'),
+    url(r'^user_view/', views.user_view, name='user_view'),
+    url(r'^create_file/', views.create_file, name='create_file'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,163 @@
+import os
+import logging
+
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import LoginForm, RegistrationForm
+from .models import UserProfile
+
+
+from devaps_ssp.settings import BASE_DIR
+# Create your views here.
+
+logger = logging.getLogger(__name__)
+
+
+def home(request):
+    logger.info('Calling Home Page Function')
+    return render(request, 'index.html')
+
+
+def admin_home(request):
+    logger.info('Calling Admin Page Function')
+    return render(request, 'admin.html', {'user': 'admin'})
+
+
+def login_page(request):
+    logger.info('Calling Login Page Function')
+    return render(request, 'login.html')
+
+
+def login(request):
+    logger.info('Calling login Function')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            try:
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                auth_user = User.objects.get(username=username)
+                auth_user.password = password
+                if auth_user is not None:
+                    if auth_user.username == 'admin' and auth_user.password == password :
+                        if auth_user.is_active:
+                            return render(request, 'admin.html', {'user': auth_user.username})
+                        else:
+                            error = 'Invalid User'
+                            return render(request, 'admin.html', {'error': error})
+                    else:
+                        if auth_user.is_active:
+                            return render(request, 'user.html', {'user': auth_user.username})
+                        else:
+                            error = 'Invalid User'
+                            return render(request, 'user.html', {'error': error})
+                else:
+                    error = 'Please enter username and password to login'
+                    return render(request, 'login.html', {'error': error, 'form': form})
+            except:
+                error = 'Invalid User Please Enter Valid User Name and Password'
+                return render(request, 'login.html', {'error': error, 'form': form})
+        else:
+            error = 'Please enter username and password to login'
+            return render(request, 'login.html', {'error': error})
+
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def logout(request):
+    logger.info('Calling Logout Function')
+    return render(request, 'login.html')
+
+
+def create_user(request):
+    logger.info('Calling Create User Function')
+    return render(request, 'create_user.html',)
+
+
+def register(request):
+    logger.info('Calling Register User Function')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            try:
+                auth_user = User()
+                auth_user.first_name = request.POST.get('first_name')
+                auth_user.last_name = request.POST.get('last_name')
+                auth_user.username = request.POST.get('username')
+                auth_user.password = request.POST.get('password')
+                auth_user.email = request.POST.get('email')
+                auth_user.is_superuser = False
+                auth_user.is_staff = True
+                auth_user.save()
+                auth_user_profile = UserProfile()
+                auth_user_profile.role = request.POST.get('role')
+                auth_user_profile.user_id = auth_user.id
+                auth_user_profile.save()
+            except:
+                error = 'Invalid Data'
+                return render(request, 'create_user.html', {'error': error})
+        else:
+            error = 'Pleae Fill the below details! All are mandatory'
+            return render(request, 'create_user.html', {'error': error})
+    else:
+        form = RegistrationForm()
+        return render(request, 'create_user.html', {'form': form})
+
+    return render(request, 'register_success.html')
+
+
+def list_users(request):
+    logger.info('Calling List All User Function')
+    auth_user = User.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'list_users.html', data)
+
+
+def update(request):
+    logger.info('Calling Edit User Function')
+    auth_user = UserProfile.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'update_user.html', data)
+
+
+def update_user(request, user_id):
+    logger.info('Calling Edit User Function')
+    if request.method == 'POST':
+        auth_user = UserProfile.objects.get(user_id=user_id)
+        auth_user.role = request.POST.get('role')
+        auth_user.save()
+
+    auth_user = UserProfile.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'update_user.html', data)
+
+
+def delete(request):
+    pass
+
+
+def delete_user(request, user_id):
+    logger.info('Calling Delete User Function')
+    auth_user = User.objects.get(pk=user_id)
+    auth_user.delete()
+    auth_user = User.objects.all()
+    data = {'object_list': auth_user}
+    return render(request, 'list_users.html', data)
+
+
+def user_view(request):
+    logger.info('Calling User View Function')
+    return render(request, 'user.html')
+
+
+def create_file(request):
+    if request.method == 'POST':
+        file = open(os.path.join(BASE_DIR, 'scripts/script.py'), 'w')
+        return HttpResponse('File has been created successfully')
+    else:
+        return HttpResponse('Something went wrong! not able to create file')
+
